a30cf3088255111ea547bd526b26a212b05c1d13
yliu84 <yliu84@studentmail.nait.ca>
2022-02-26 14:39:01 -0700

    Security headers added

--- API/Program.cs
+++ API/Program.cs
@@ -20,11 +20,51 @@ builder.Services.AddIdentityServices(builder.Configuration);
 // Configure the http request pipeline
 var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseXContentTypeOptions();
+app.UseReferrerPolicy(opt => opt.NoReferrer());
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+app.UseXfo(opt => opt.Deny());
+app.UseCsp(opt => opt
+    .BlockAllMixedContent()
+    .StyleSources(s => s.Self().CustomSources(
+        "https://fonts.googleapis.com",
+        "sha256-/epqQuRElKW1Z83z1Sg8Bs2MKi99Nrq41Z3fnS2Nrgk=",
+        "sha256-2aahydUs+he2AO0g7YZuG67RGvfE9VXGbycVgIwMnBI=",
+        "sha256-+oGcdj5BhO6SoiIGYIkPOMYi7d2h2Pp/bkJLBfYL+kk="
+    ))
+    .FontSources(s => s.Self().CustomSources(
+        "https://fonts.gstatic.com", "data:"
+    ))
+    .FormActions(s => s.Self())
+    .FrameAncestors(s => s.Self())
+    .ImageSources(s => s.Self().CustomSources(
+        "https://res.cloudinary.com",
+        "https://www.facebook.com",
+        "https://platform-lookaside.fbsbx.com",
+        "data:"
+        ))
+    .ScriptSources(s => s.Self()
+        .CustomSources(
+            "sha256-HIgflxNtM43xg36bBIUoPTUuo+CXZ319LsTVRtsZ/VU=",
+            "https://connect.facebook.net",
+            "sha256-3x3EykMfFJtFd84iFKuZG0MoGAo5XdRfl3rq3r//ydA="
+        ))
+);
+
 if (app.Environment.IsDevelopment())
 {
     // app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+}
+else
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 // app.UseHttpsRedirection();
 
